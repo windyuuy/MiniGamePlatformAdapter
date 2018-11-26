f6de5fb10338b22ada1d3e00fbb5fbe2206d94ed
JasonWu <>
2018-11-26 11:25:53 +0800

    [refact][red] 重构代码

--- packconfigs/packconfigs.ts
+++
@@ -1,8 +0,0 @@
-import { PackConfig } from "../frame/PackConfig";
-import { oppoapi } from "../plugins/oppo/config";
-import { qqplay } from "../plugins/qqplay/config";
-
-export const packconfigs: PackConfig[] = [
-	oppoapi,
-	qqplay,
-]
