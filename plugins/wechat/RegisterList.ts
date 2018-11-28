154945fa7041f2903034fee0a5d4b0bfc659bac9
windy <kugoo11v12@126.com>
2018-11-28 11:40:47 +0800

    [refact][red] update wx RegisterList

--- plugins/wechat/RegisterList.ts
+++ plugins/wechat/RegisterList.ts
@@ -1,4 +1,15 @@
 namespace WechatGDK {
 	export class RegisterList extends GDK.ModuleClassMap {
+		Advert = Advert
+		// GameInfo = GameInfo
+		User = User
+		Pay = Pay
+		Share = Share
+		// SystemInfo = SystemInfo
+		UserData = UserData
+		Customer = Customer
+		Widgets = Widgets
+		SubContext = SubContext
+		Support = Support
 	}
 }
