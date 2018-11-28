51ff8cb97af67d7e7c0d653f0101f8b14b9f353a
windy <kugoo11v12@126.com>
2018-11-28 16:16:41 +0800

    [feature][red] 支持develop充值

--- plugins/develop/Pay.ts
+++ plugins/develop/Pay.ts
@@ -1,46 +1,40 @@
 
 namespace DevelopGDK {
-	const log = new GDKLIB.Log({ tags: ['api'] })
+	const log = new GDKLIB.Log({ tags: ['DevelopPay'] })
 	export class Pay implements GDK.IPay {
-		api?: GDK.UserAPI
 
 		payPurchase(config: GDK.PayItemInfo): Promise<GDK.PayResult> {
 			const ret = new GDK.RPromise<GDK.PayResult>()
 
-			const info = this.api.gameInfo
-			const env = info.isPayInSandbox ? 1 : 0
-			const successCode = 999999
-			wx.requestMidasPayment({
-				mode: "game",
-				env: env,
-				offerId: info.offerId,
-				currencyType: config.currencyUnit || "CNY",
-				platform: this.api.systemInfo.system,
-				zoneId: "1",
-				buyQuantity: config.money * 10,
-				success: () => {
-					log.info("微信充值成功", config)
-					ret.success({
-						result: {
-							errCode: successCode,
-						},
-						extra: { errCode: successCode, state: GDK.OrderState.ok },
-					})
-				},
-				fail: (res: { errCode: number }) => {
-					if (res.errCode == 1) {
-						log.info("微信充值取消", res, config)
-						ret.fail(GDK.GDKResultTemplates.make(GDK.GDKErrorCode.API_PAY_CANCEL))
-					} else {
-						log.warn("微信充值失败", res, config)
-						ret.fail(GDK.GDKResultTemplates.make(GDK.GDKErrorCode.API_PAY_FAILED, {
-							data: {
-								extra: res
-							}
-						}))
+			const ok = confirm("确定充值吗？\n" + JSON.stringify(config))
+			if (ok) {
+				log.info("模拟充值成功", config)
+				ret.success({
+					result: {
+						errCode: 0,
+					},
+					extra: { errCode: 0, state: GDK.OrderState.ok },
+				})
+			} else {
+				const cancel = confirm("确定取消充值吗？\n(点确定取消充值，点取消充值失败！)")
+				if (cancel) {
+					const res = {
+						errCode: -1,
 					}
+					log.info("模拟充值取消", res, config)
+					ret.fail(GDK.GDKResultTemplates.make(GDK.GDKErrorCode.API_PAY_CANCEL))
+				} else {
+					const res = {
+						errCode: 9999,
+					}
+					log.warn("模拟充值失败", res, config)
+					ret.fail(GDK.GDKResultTemplates.make(GDK.GDKErrorCode.API_PAY_FAILED, {
+						data: {
+							extra: res
+						}
+					}))
 				}
-			})
+			}
 
 			return ret.promise
 		}
