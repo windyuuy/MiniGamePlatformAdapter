fb368eb89b3a9e3656b1e6e3cb9ec44fa15a861c
windy <kugoo11v12@126.com>
2018-11-29 10:36:54 +0800

    [refact][red] refact SubContext

---
+++ framework/common/SubContext.ts
@@ -0,0 +1,28 @@
+
+namespace GDK {
+	class SimpleEvent<T> extends GDKLIB.SimpleEvent<T> { }
+
+	class OpenDataContext implements IOpenDataContext {
+		protected _event: SimpleEvent<PrimitiveMap> = null
+		constructor(event: SimpleEvent<PrimitiveMap>) {
+			this._event = event
+		}
+
+		postMessage(message: PrimitiveMap) {
+			this._event.emit(message)
+		}
+	}
+
+	export class SubContextBase implements ISubContext {
+		protected _event: SimpleEvent<PrimitiveMap> = new SimpleEvent()
+		protected _context: OpenDataContext = new OpenDataContext(this._event)
+
+		onMessage(callback: (message: PrimitiveMap) => void) {
+			return this._event.on(callback)
+		}
+
+		getOpenDataContext(): IOpenDataContext {
+			return this._context
+		}
+	}
+}
--- plugins/develop/SubContext.ts
+++ plugins/develop/SubContext.ts
@@ -1,28 +1,4 @@
 
 namespace DevelopGDK {
-	class SimpleEvent<T> extends GDKLIB.SimpleEvent<T> { }
-
-	class OpenDataContext implements GDK.IOpenDataContext {
-		protected _event: SimpleEvent<GDK.PrimitiveMap> = null
-		constructor(event: SimpleEvent<GDK.PrimitiveMap>) {
-			this._event = event
-		}
-
-		postMessage(message: GDK.PrimitiveMap) {
-
-		}
-	}
-
-	export class SubContext implements GDK.ISubContext {
-		protected _event: SimpleEvent<GDK.PrimitiveMap> = new SimpleEvent()
-		protected _context: OpenDataContext = new OpenDataContext(this._event)
-
-		onMessage(callback: (message: GDK.PrimitiveMap) => void) {
-			return this._event.on(callback)
-		}
-
-		getOpenDataContext(): GDK.IOpenDataContext {
-			return this._context
-		}
-	}
+	export class SubContext extends GDK.SubContextBase { }
 }
