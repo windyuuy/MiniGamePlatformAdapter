a08ffd5f692228737f6f1ae9e8f53e3f2e9c7745
windy <kugoo11v12@126.com>
2018-11-29 16:46:23 +0800

    [refact][red] support set default gdk instance

--- framework/export/export.ts
+++ framework/export/export.ts
@@ -1,1 +1,8 @@
 declare let gdk: GDK.UserAPI
+namespace GDK {
+
+	export function setDefaultGdk(api: UserAPI) {
+		slib.assert(!gdk, '-[GDK] default gdk instance shall not be set twice')
+		gdk = api
+	}
+}
