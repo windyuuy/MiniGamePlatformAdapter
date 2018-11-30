78ec46dfd0846aa67a5612061a165ca59a38ee31
windy <kugoo11v12@126.com>
2018-11-30 18:13:21 +0800

    [feature][red] 支持事件分发库

--- libs/SimpleEvent.ts
+++ libs/SimpleEvent.ts
@@ -1,11 +1,12 @@
-namespace GDKLIB {
+namespace slib {
+	export type EventHandler<T> = (message: T) => void
 	export class SimpleEvent<T> {
-		protected _callbacks: ((message: T) => void)[] = []
-		on(callback) {
+		protected _callbacks: EventHandler<T>[] = []
+		on(callback: EventHandler<T>) {
 			this._callbacks.push(callback)
 		}
 
-		off(callback) {
+		off(callback: EventHandler<T>) {
 			this._callbacks.splice(this._callbacks.indexOf(callback), 1)
 		}
 
@@ -13,6 +14,34 @@ namespace GDKLIB {
 			this._callbacks.forEach((callback) => {
 				callback(value)
 			})
+		}
+	}
+
+	export class SEvent<T>{
+		protected _events: { [key: string]: SimpleEvent<T> } = {}
+
+		on(key: string, callback: EventHandler<T>) {
+			if (!this._events[key]) {
+				this._events[key] = new SimpleEvent<T>()
+			}
+			const event = this._events[key]
+			event.on(callback)
+		}
+
+		off(key: string, callback: EventHandler<T>) {
+			if (callback == undefined) {
+				this._events[key] = undefined
+			} else {
+				const event = this._events[key]
+				event.off(callback)
+			}
+		}
+
+		emit(key: string, value: T) {
+			if (this._events[key]) {
+				const event = this._events[key]
+				event.emit(value)
+			}
 		}
 	}
 }
