86e4f39018d73fac713e3697519c84c0daa78d2b
windy <kugoo11v12@126.com>
2018-12-20 12:07:45 +0800

    [step][red] update wx.d.ts

--- src/plugins/wechat/wx.d.ts
+++ src/plugins/wechat/wx.d.ts
@@ -2226,7 +2226,41 @@ declare namespace wx {
 		 */
 	export function setUserCloudStorage(obj: { KVDataList: KVData[], success?: Function, fail?: Function, complete?: Function })
 
-	export function openCustomerServiceConversation(object: Object)
+	export class CustomerServiceConversationParams {
+		/**
+		 * 	''	否	会话来源
+		 */
+		sessionFrom?: string
+		/**
+		 * 	false	否	是否显示会话内消息卡片，设置此参数为 true，用户进入客服会话之后会收到一个消息卡片，通过以下三个参数设置卡片的内容
+		 */
+		showMessageCard?: boolean
+		/**
+		 * 	''	否	会话内消息卡片标题
+		 */
+		sendMessageTitle?: string
+		/**
+		 * 	''	否	会话内消息卡片路径
+		 */
+		sendMessagePath?: string
+		/**
+		 * 	''	否	会话内消息卡片图片路径
+		 */
+		sendMessageImg?: string
+		/**
+		 *  否	接口调用成功的回调函数
+		 */
+		success?: Function
+		/**
+		 *  否	接口调用失败的回调函数
+		 */
+		fail?: Function
+		/**
+		 *  否	接口调用结束的回调函数（调用成功、失败都会执行）
+		 */
+		complete?: Function
+	}
+	export function openCustomerServiceConversation(object: CustomerServiceConversationParams)
 
 	export class ShowLoadingParams {
 		/** 提示的内容 */
