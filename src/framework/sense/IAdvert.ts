1b1a62e818a5bb029fb8e0eb5464ef289f2b796b
windy <kugoo11v12@126.com>
2018-12-28 15:01:29 +0800

    [step][blue] 完善广告日志

--- src/framework/sense/IAdvert.ts
+++ src/framework/sense/IAdvert.ts
@@ -80,7 +80,10 @@ namespace GDK {
 	}
 
 	export interface IAdvert extends IModule {
-		/** 创建激励视频广告对象 */
+		/**
+		 * 是个单例
+		 * 创建激励视频广告对象
+		 */
 		createRewardedVideoAd(params: VideoAdCreateParam): IRewardedVideoAd
 
 		/** 创建条幅广告对象 */
