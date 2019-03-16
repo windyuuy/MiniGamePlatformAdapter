f4609f53408b723f93427880d6b739ccd4fde8b3
windy <kugoo11v12@126.com>
2019-03-16 18:12:19 +0800

    [fixbug][blue] 完善微信视频广告库版本检查

--- src/plugins/wechat/Advert.ts
+++ src/plugins/wechat/Advert.ts
@@ -6,6 +6,13 @@ namespace WechatGDK {
 			/** 广告单元 id */
 			adUnitId: string
 		}): GDK.IRewardedVideoAd {
+
+			if (!this.compareVersion(gdk.SDKVersion, "2.0.4")) {
+				//不是最新版本直接下发奖励
+				console.log("微信不是最新版本，请升级！(微信基础库 2.0.4 开始支持，低版本需做兼容处理。)")
+				return null
+			}
+
 			return wx.createRewardedVideoAd(params)
 		}
 
@@ -39,5 +46,30 @@ namespace WechatGDK {
 			}
 			return wx.createBannerAd(params2)
 		}
+
+
+        /**
+         * 对比版本
+         * - v1>v2 -> true
+         * - v2>v1 -> false
+         * - v1==v2 -> dv
+         */
+		protected compareVersion(v1: string, v2: string, dv: boolean = true) {
+			let v1s = v1.split('.')
+			let v2s = v2.split('.')
+			for (let index = 0; index < Math.min(v1s.length, v2s.length); index++) {
+				let a1 = v1s[index]
+				let a2 = v2s[index]
+				if (a1 != a2) {
+					return a1 > a2
+				}
+			}
+			if (v1s.length != v2s.length) {
+				return v1s.length > v2s.length
+			}
+			// 全等，返回 dv
+			return dv
+		}
+
 	}
 }
