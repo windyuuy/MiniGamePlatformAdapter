9e358215123c633383c2e9bb21aaa7fea1c81b00
JasonWu <wjs@mrglee.com>
2019-03-25 16:55:04 +0800

    [fixbug][red] string忘记转换成json对象

--- src/plugins/app/APISystem.ts
+++ src/plugins/app/APISystem.ts
@@ -7,8 +7,8 @@ namespace AppGDK {
 			if (gdkjsb.bridge == undefined) {
 				callback({ left: 0, right: 0, top: 0, bottom: 0 });
 			} else {
-				gdkjsb.bridge.callAction("DisplayCutout:getSafeArea", "{}", (data: any) => {
-					callback(data);
+				gdkjsb.bridge.callAction("DisplayCutout:getSafeArea", "{}", (data: string) => {
+					callback(JSON.parse(data));
 				})
 			}
 		}
