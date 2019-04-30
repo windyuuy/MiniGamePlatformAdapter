8b24f10d4f18ec5af27c73c8ddf6fc1fc46a2d75
windy <kugoo11v12@126.com>
2019-04-30 15:37:57 +0800

    [modify][red] 修改代码提示

--- src/plugins/develop/APISystem.ts
+++ src/plugins/develop/APISystem.ts
@@ -1,11 +1,13 @@
 
 namespace DevelopGDK {
 
+	const devlog = Common.devlog
+
 	export class APISystem extends GDK.APISystemBase {
 		init() {
-			console.error("当前 gdk.js 为develop版，仅适用于发开自测流程，没有特殊需求请勿用于发布到微信等平台上！！！")
+			devlog.error("当前 gdk.js 为develop版，仅适用于发开自测流程，没有特殊需求请勿用于发布到微信等平台上！！！")
 			if (window['wx'] && window['wx'].authorize) {
-				console.error("当前平台疑似微信平台，请确认发布配置是否正确")
+				devlog.error("当前平台疑似微信平台，当前 gdk.js 为develop版，请确认发布配置是否正确")
 			}
 		}
 	}
