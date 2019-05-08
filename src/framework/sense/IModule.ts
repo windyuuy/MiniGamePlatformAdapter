8c541a1179aff571bca4568d17f98bbd615cb32e
windy <kugoo11v12@126.com>
2019-05-08 17:35:52 +0800

    [step][red] 更新文档

--- src/framework/sense/IModule.ts
+++ src/framework/sense/IModule.ts
@@ -1,7 +1,13 @@
 namespace GDK {
 	export interface IModule {
 		api?: UserAPI
+		/**
+		 * 模块初始化入口
+		 */
 		init?(data?: any)
+		/**
+		 * 模块传入配置初始化入口
+		 */
 		initWithConfig?(info: GDKConfig)
 	}
 }
