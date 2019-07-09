ce525a6327ab5f30f2ae4df7a39be8bc7393f2d8
zfp <zfp@mrglee.com>
2019-07-09 16:32:13 +0800

    no message

--- src/plugins/vivo/Advert.ts
+++ src/plugins/vivo/Advert.ts
@@ -1,5 +1,111 @@
 
 namespace VIVOGDK {
+	const devlog = Common.devlog
+
+	class VideoAd implements GDK.IRewardedVideoAd {
+		protected _loadFuncList: Function[] = []
+		protected _errorFuncList: Function[] = []
+		protected _closeFuncList: Function[] = []
+
+		protected _isLoad: boolean = false;
+		protected _isShowing: boolean = false
+		protected _isAdInited: boolean = false;
+		protected _videoAd = null;
+
+		adUnitId: string
+		constructor(
+			params: {
+				adUnitId: string
+			}
+		) {
+
+		}
+
+		protected _available: boolean = false
+		get isAvailable() {
+			return this._available
+		}
+		async checkAvailable?(): Promise<boolean> {
+			return this._available
+		}
+
+		async load(): Promise<void> {
+			console.log("gdk oppo load videoad")
+			const ret = new GDK.RPromise<void>()
+			return ret.promise
+		}
+
+		async show(): Promise<void> {
+			console.log("gdk oppo show videoad")
+			const ret = new GDK.RPromise<void>()
+			return ret.promise
+		}
+
+		onLoad(callback: Function) {
+			this._loadFuncList.push(callback)
+		}
+		offLoad(callback: Function) {
+			this._loadFuncList.splice(this._loadFuncList.indexOf(callback), 1)
+		}
+
+		onError(callback: (res: GDK.RewardedVideoAdOnErrorParam) => void) {
+			this._errorFuncList.push(callback)
+		}
+		offError(callback: Function) {
+			this._errorFuncList.splice(this._errorFuncList.indexOf(callback), 1)
+		}
+
+		onClose(callback: Function) {
+			this._closeFuncList.push(callback)
+		}
+		offClose(callback: Function) {
+			this._closeFuncList.splice(this._closeFuncList.indexOf(callback), 1)
+		}
+	}
+
+	class BannerAd implements GDK.IBannerAd {
+		adUnitId?: string
+
+		protected _style: GDK.BannerStyleAccessor = new GDK.BannerStyleAccessor()
+		get style(): GDK.BannerStyleAccessor {
+			return this._style
+		}
+		set style(value: GDK.BannerStyleAccessor) {
+			this._style = value
+		}
+
+		constructor(params: { posId: string, style?: GDK.BannerStyle }) {
+			this.adUnitId = params.posId || "35668"; // 默认是彩虹岛的banner
+		}
+
+		show(): Promise<void> {
+			const ret = new GDK.RPromise<void>()
+			ret.success(undefined)
+			return ret.promise
+		}
+		async hide() {
+		}
+		async destroy() {
+		}
+		onResize(callback: Function) {
+			devlog.warn('oppo bannerAd dose not support resize')
+		}
+		offResize(callback: Function) {
+			devlog.warn('oppo bannerAd dose not support resize')
+		}
+		onLoad(callback: Function) {
+
+		}
+
+		offLoad(callback: Function) {
+
+		}
+		onError(callback: (code: number, msg: string) => void) {
+		}
+		offError(callback: Function) {
+		}
+	}
+
 	export class Advert implements GDK.IAdvert {
 
 		createRewardedVideoAd(params: {
@@ -10,7 +116,7 @@ namespace VIVOGDK {
 			if (qg.getSystemInfoSync().platformVersionCode >= 1041) {
 				return qg.createRewardedVideoAd({ posId: params.adUnitId })
 			} else
-				return null
+				return new VideoAd(params)
 		}
 
 		createBannerAd(params: {
@@ -23,7 +129,7 @@ namespace VIVOGDK {
 			if (qg.getSystemInfoSync().platformVersionCode >= 1031) {
 				return qg.createBannerAd({ posId: params.adUnitId, style: {} })
 			} else
-				return null
+				return new BannerAd({ posId: params.adUnitId, style: {} })
 		}
 	}
 }
