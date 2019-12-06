5c60b7a5e03533ad03433255a960fda11ebbc5d7
JasonWu <wjs@mrglee.com>
2019-12-06 13:18:35 +0800

    [fixbug][red] 修复信息流广告判断错误bug

--- src/plugins/app/Advert.ts
+++ src/plugins/app/Advert.ts
@@ -133,7 +133,7 @@ namespace AppGDK {
 		}
 
 		get supportFeedAd(): boolean {
-			return gdkjsb.country == "zh" && nativeHelper.checkActionExist("ironsrc:IronSource.createFeedAd")
+			return gdkjsb.country == "CN" && nativeHelper.checkActionExist("ironsrc:IronSource.createFeedAd")
 		}
 
 		createFeedAd?(params: GDK.FeedAdCreateParam): GDK.IFeedAd {
