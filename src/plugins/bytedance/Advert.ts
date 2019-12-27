542197b41db97841792198af9fe637b715ab3447
windy <kugoo11v12@126.com>
2019-12-27 14:13:14 +0800

    [fixbug][blue] 修正头条广告版本判断

--- src/plugins/bytedance/Advert.ts
+++ src/plugins/bytedance/Advert.ts
@@ -7,9 +7,9 @@ namespace BytedanceGDK {
 			adUnitId: string
 		}): GDK.IRewardedVideoAd {
 
-			if (!this.compareVersion(gdk.SDKVersion, "2.0.4")) {
+			if (!this.compareVersion(gdk.SDKVersion, "1.3.0")) {
 				//不是最新版本直接下发奖励
-				console.log("头条不是最新版本，请升级！(头条基础库 2.0.4 开始支持，低版本需做兼容处理。)")
+				console.log("头条不是最新版本，请升级！(头条基础库 1.3.0 开始支持，低版本需做兼容处理。)")
 				return null
 			}
 
