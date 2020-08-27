a9ea4e07c1147dfdd88554d92cadef187b8525fd
windy <kugoo11v12@126.com>
2020-08-27 13:24:57 +0800

    [modify] 梳理支付流程，建立标准

--- src/plugins/appshare/pay/MeituAppPayFlow.ts
+++ src/plugins/appshare/pay/MeituAppPayFlow.ts
@@ -34,12 +34,111 @@ namespace AppShare.PayFlow.MeituAppPayFlow {
         gameSign?: string;
     }
 
+    export class MeituPayRequests extends GDK.PayFlow.PayRequestsNormal {
+
+		/**
+		 * 请求服务器生成订单
+		 * @param data 
+		 * @param callback 
+		 * @param modal 
+		 * @param errorCallback 
+		 */
+        orderGenOrder(
+            data: GenOrderParams,
+            callback: (data: {
+                succeed: boolean,
+                code: 0 | number,
+                message: "success" | string,
+                data: OrderInfo, //订单信息
+            }) => void,
+            modal: boolean = false,
+            errorCallback: (error: any, retry: () => void) => void = null
+        ) {
+            (async () => {
+                let path = 'mt/createOrder'
+                this.client.request(path, data, (data) => {
+                    if (data.succeed && (data.data instanceof Object)) {
+                        data.data.time = data.data.createTime
+                    }
+
+                    if (data.code == 800) {
+                        payDeps.api.showAlert({ okLabel: "确定", content: data.message, title: "提示" })
+                    }
+
+                    callback(data);
+                }, { modal: modal, errorCallback: errorCallback })
+            })();
+        }
+
+		/**
+		 * 检查订单状态
+		 * @param data 
+		 * @param callback 
+		 * @param modal 
+		 * @param errorCallback 
+		 */
+        orderCheckOrderState(
+            data: CheckOrderStateParams,
+            callback: (data: {
+                succeed: boolean,
+                code: 0 | number,
+                message: "success" | string,
+                data: 0 | 1//0未充值 1已充值
+            }) => void, modal: boolean = false, errorCallback: (error: any, retry: () => void) => void = null) {
+            let path = "order/getOrderStatus"
+            this.client.request(path, data, (data) => {
+                if (payDeps.api.pluginName == "develop") {
+                    // 浏览器上模拟测试充值
+                    data.succeed = true
+                    data.code = 0
+                    data.data = Math.random() < 0.9 ? 1 : 2
+                }
+                callback(data);
+            }, { modal: modal, errorCallback: errorCallback })
+        }
+
+		/**
+		 * 请求历史差异订单
+		 * @param data 
+		 * @param callback 
+		 * @param modal 
+		 * @param errorCallback 
+		 */
+        orderReqDiffOrderList(
+            data: ReqDiffOrderListParams,
+            callback: (data: {
+                succeed: boolean,
+                code: 0 | number,
+                message: "success" | string,
+                data: OrderInfoRaw[], // 历史订单列表
+            }) => void,
+            modal: boolean = false,
+            errorCallback: (error: any, retry: () => void) => void = null
+        ) {
+            let path = "order/getOrderList"
+
+            this.client.request(path, data, (data) => {
+                // if (true) {
+                //     data.succeed = true
+                //     data.code = 0
+                //     data.data = [
+                //         { createTime: 1530360114000, goodsId: 1, id: null, outTradeNo: "20002_209_1530360114388", quantity: 1, state: 1, time: 1530360114000, title: "com.farm.p60", userId: 209 }
+                //     ]
+                // }
+                callback(data);
+            }, { modal: modal, errorCallback: errorCallback })
+        }
+    }
+
 	/**
 	 * YYB app渠道
      * - 针对 YYB 渠道支付在安卓平台特有的一些问题等进行定制
 	 */
     export class PayFlow extends GDK.PayFlow.PayInApp.PayFlow {
         payFlowName = "MeituAppPayFlow"
+
+        /** 重载支付网络请求 */
+        payNetClient = new MeituPayRequests()
 
         /**
 		 * 包装调起原生支付的参数
--- src/plugins/wechat/pay/PayCommon.ts
+++ src/plugins/wechat/pay/PayCommon.ts
@@ -1,24 +1,22 @@
 
-namespace AppShare.PayFlow {
+namespace WechatGDK.PayFlow {
+
+	export const mdebug = (window as any)['wdebug'] && true
+	export const log = new slib.Log({ time: false, tags: ['[PayFlow]'] })
 
 	export type IPayFlow = GDK.PayFlow.IPayFlow
-	export type PayFlowStatus = GDK.PayFlow.PayFlowStatus
 	export type Parent = GDK.PayFlow.Parent
 	export type PayWay = GDK.PayFlow.PayWay
 	export const PayInApp = GDK.PayFlow.PayInApp
-	export const PayInAppWithAutoMakeup = GDK.PayFlow.PayInAppWithAutoMakeup
-	export const PayInsideLocalV2 = GDK.PayFlow.PayInsideLocalV2
+	export type OrderInfo = GDK.PayFlow.OrderInfo
 	export const payDeps = GDK.PayFlow.payDeps
 	export type PaymentParamsOptions = GDK.PayFlow.PaymentParamsOptions
 	export type PaymentParams = GDK.PayFlow.PaymentParams
 	export type PaymentSuccessCallback = GDK.PayFlow.PaymentSuccessCallback
 	export type RechargeConfigRow = GDK.PayFlow.RechargeConfigRow
 	export type OrderRecordExported = GDK.PayFlow.OrderRecordExported
-	export type MakeupOrdersResult = GDK.PayFlow.MakeupOrdersResult
-	export type OrderInfo = GDK.PayFlow.OrderInfo
-	export type OrderInfoRaw = GDK.PayFlow.OrderInfoRaw
 	export type GenOrderParams = GDK.PayFlow.GenOrderParams
 	export type CheckOrderStateParams = GDK.PayFlow.CheckOrderStateParams
 	export type ReqDiffOrderListParams = GDK.PayFlow.ReqDiffOrderListParams
-
+	export type OrderInfoRaw = GDK.PayFlow.OrderInfoRaw
 }
