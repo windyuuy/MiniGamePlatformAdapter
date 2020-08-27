c20c584b1e7a9c96f2ec030ed11fd5685a248a0a
jasonwu <birnfly@qq.com>
2020-08-27 12:40:42 +0800

    [modify] webview 删除没必要的模块

--- src/plugins/webview/RegisterList.ts
+++ src/plugins/webview/RegisterList.ts
@@ -1,10 +1,7 @@
 namespace WebViewGDK {
 	export class RegisterList extends GDK.ModuleClassMap {
-		Advert = Advert
 		GameInfo = GameInfo
 		User = User
-		Pay = Pay
-		Share = Share
 		SystemInfo = SystemInfo
 		UserData = UserData
 		Customer = Customer
