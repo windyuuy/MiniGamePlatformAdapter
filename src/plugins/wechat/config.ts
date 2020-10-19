3008030bb6bb7cc14b8ba48d7995139b2ec12b12
windy <kugoo11v12@126.com>
2020-10-19 10:44:16 +0800

    [improve] 升级版本，支持头条小游戏复用微信小游戏代码

--- src/plugins/wechat/config.ts
+++ src/plugins/wechat/config.ts
@@ -3,7 +3,7 @@ namespace WechatGDK {
 	// default config
 	GDK.gdkManager.registPluginConfig({
 		name: 'wechat',
-		version: '1.0.2',
+		version: '1.0.3',
 		platform: "wechatgame",
 		platformLocale: "微信",
 		register: RegisterList,
