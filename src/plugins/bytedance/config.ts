8608bb1f383ca87f9f6291ef4374d483ea161c3c
xpf <xupengfei@jktom.com>
2020-10-30 09:15:53 +0800

    [step] 微信抖音版本号升级

--- src/plugins/bytedance/config.ts
+++ src/plugins/bytedance/config.ts
@@ -3,7 +3,7 @@ namespace BytedanceGDK {
 	// default config
 	GDK.gdkManager.registPluginConfig({
 		name: 'bytedance',
-		version: '1.0.3',
+		version: '1.0.4',
 		platform: "bytedance",
 		platformLocale: "头条",
 		register: RegisterList,
--- src/plugins/wechat/config.ts
+++ src/plugins/wechat/config.ts
@@ -3,7 +3,7 @@ namespace WechatGDK {
 	// default config
 	GDK.gdkManager.registPluginConfig({
 		name: 'wechat',
-		version: '1.0.3',
+		version: '1.0.4',
 		platform: "wechatgame",
 		platformLocale: "微信",
 		register: RegisterList,
